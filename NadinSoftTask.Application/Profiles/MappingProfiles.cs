dfa7add76ef3f0979b334f4b01f141b73b88a9f2
NaderWD <Nader.webdevop@gmail.com>
2024-12-09 17:40:25 +0330

    IMapper

---
+++ NadinSoftTask.Application/Profiles/MappingProfiles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using NadinSoftTask.Domain.Models;
+using NadinSoftTask.Domain.ViewModels;
+
+namespace NadinSoftTask.Application.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<CreateProductVM, Product>();
+            CreateMap<UpdateProductVM, Product>();
+            CreateMap<ProductListVM, Product>();
+        }
+    }
+}
