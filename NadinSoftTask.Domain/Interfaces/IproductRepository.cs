550eb2b8245d30bb8f22f317f35eb3db0856ea4d
NaderWD <Nader.webdevop@gmail.com>
2024-12-09 17:30:23 +0330

    Interfaces Domain

---
+++ NadinSoftTask.Domain/Interfaces/IproductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NadinSoftTask.Domain.Models;
+using NadinSoftTask.Domain.ViewModels;
+
+namespace NadinSoftTask.Domain.Interfaces
+{
+    public interface IproductRepository
+    {
+        IEnumerable<ProductListVM> GetAll();
+        Product GetById(int id);
+        void Add(Product product);
+        void Update(Product product);
+        void DeleteById(int id);
+
+    }
+}
