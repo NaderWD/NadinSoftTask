d27987ca95ab400211c1b4b36ea3c45218f63bcd
NaderWD <Nader.webdevop@gmail.com>
2024-12-09 18:41:32 +0330

    Connection String And Program.cs Config

---
+++ NadinSoftTask.Presentation.WebAPI/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using NadinSoftTask.Infra.Data.Context;
+using NadinSoftTask.Infra.Ioc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connection = builder.Configuration.GetConnectionString("NadinTaskConnection");
+builder.Services.AddDbContext<NadinTaskDB>(a => a.UseSqlServer(connection));
+DependencyContainer.RegisterServices(builder.Services);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
