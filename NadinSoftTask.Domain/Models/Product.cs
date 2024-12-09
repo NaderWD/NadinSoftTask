8db37654b0d90a4425485fd389246158b79b95a4
NaderWD <Nader.webdevop@gmail.com>
2024-12-09 19:17:54 +0330

    Problem Solved And Migration Added

--- NadinSoftTask.Domain/Models/Product.cs
+++ NadinSoftTask.Domain/Models/Product.cs
@@ -13,8 +13,8 @@ namespace NadinSoftTask.Domain.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime ProduceDate { get; set; }
-        public PhoneAttribute ManufacturePhone { get; set; }
-        public EmailAddressAttribute ManufactureEmail { get; set; }
+        public string ManufacturePhone { get; set; }
+        public string ManufactureEmail { get; set; }
         public bool IsAvailable { get; set; }
     }
 }
---
+++ NadinSoftTask.Infra.Data/Migrations/20241209154714_Init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NadinSoftTask.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProduceDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ManufacturePhone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ManufactureEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
