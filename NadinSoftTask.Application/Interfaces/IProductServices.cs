c85631c277a47b97acb83f45c6d20c7860963234
NaderWD <Nader.webdevop@gmail.com>
2024-12-09 18:20:58 +0330

    Product Service

---
+++ NadinSoftTask.Application/Interfaces/IProductServices.cs
@@ -0,0 +1,18 @@
+﻿using NadinSoftTask.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadinSoftTask.Application.Interfaces
+{
+    public interface IProductServices
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProductById(int id);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProductById(int id);
+    }
+}
---
+++ NadinSoftTask.Application/Services/ProductService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using NadinSoftTask.Application.Interfaces;
+using NadinSoftTask.Application.ViewModels;
+using NadinSoftTask.Domain.Interfaces;
+using NadinSoftTask.Domain.Models;
+
+namespace NadinSoftTask.Application.Services
+{
+    public class ProductService : IProductServices
+    {
+        private readonly IproductRepository _repository;
+        private readonly IMapper _mapper;
+        public ProductService(IproductRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public void AddProduct(Product product)
+        {
+            _repository.Add(product);
+        }
+
+        public void DeleteProductById(int id)
+        {
+            _repository.DeleteById(id);
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            var model = _repository.GetAll();
+            var result = _mapper.Map<IEnumerable<Product>>(model);
+            return result;
+        }
+
+        public Product GetProductById(int id)
+        {
+            var model = _repository.GetById(id);
+            return model;
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _repository.Update(product);
+        }
+    }
+}
