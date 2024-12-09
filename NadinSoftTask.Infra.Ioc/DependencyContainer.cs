144fb87f61314c30b4650ad581e06daec6858f73
NaderWD <Nader.webdevop@gmail.com>
2024-12-09 18:29:01 +0330

    IOC And Dependency Container Done

---
+++ NadinSoftTask.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NadinSoftTask.Domain.Interfaces;
+using NadinSoftTask.Infra.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NadinSoftTask.Application.Interfaces;
+using NadinSoftTask.Application.Services;
+
+namespace NadinSoftTask.Infra.Ioc
+{
+    public class DependencyContainer 
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            service.AddScoped<IproductRepository, ProductRepository>();
+            service.AddScoped<IProductServices, ProductService>();
+        }
+    }
+}
