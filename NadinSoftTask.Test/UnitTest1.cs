59a5ad5d91306f23d6ef6ef971dc40b5cc25addd
NaderWD <Nader.webdevop@gmail.com>
2024-12-10 15:16:23 +0330

    Test Project And Moq And Shouldly packages

---
+++ NadinSoftTask.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NadinSoftTask.Domain.Models;
+using Xunit;
+
+namespace NadinSoftTask.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
